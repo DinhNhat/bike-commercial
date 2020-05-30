98484a3188ff08a2735b8d4b4182401e3e7cbce7
Nhat Dinh <nguyendinhnat206@gmail.com>
2020-05-30 19:27:06 -0400

    The second commit

--- src/main/java/com/nhatdinhnguyen/bicycleproject/db/repo/CategoryRepository.java
+++ src/main/java/com/nhatdinhnguyen/bicycleproject/db/repo/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.nhatdinhnguyen.bicycleproject.db.repo;
 
-public interface CategoryRepository {
+import com.nhatdinhnguyen.bicycleproject.db.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
 }
