bc4a88e7034aaa913e9714ba01d3822b4678965f
Nhat Dinh <nguyendinhnat206@gmail.com>
2020-05-30 20:33:10 -0400

    Test brand Enity with the repository interface

--- src/main/java/com/nhatdinhnguyen/bicycleproject/BicycleProjectApplication.java
+++ src/main/java/com/nhatdinhnguyen/bicycleproject/BicycleProjectApplication.java
@@ -1,13 +1,26 @@
 package com.nhatdinhnguyen.bicycleproject;
 
+import com.nhatdinhnguyen.bicycleproject.db.domain.Brand;
+import com.nhatdinhnguyen.bicycleproject.db.repo.BrandRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BicycleProjectApplication {
+public class BicycleProjectApplication implements CommandLineRunner {
+
+	@Autowired
+	private BrandRepository brandRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BicycleProjectApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("************Original Brand***************");
+		this.brandRepository.findAll().forEach(System.out::println);
+		System.out.println("The total number of brand: " + this.brandRepository.count());
+	}
 }
